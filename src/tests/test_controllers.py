1abe569e0dd3faeea78e738e6a4ef1d3e197472b
Aaron Crosman <acrosman@gmail.com>
2015-04-18 14:25:04 -0400

    Adding controller test for request_patterns method.

--- src/tests/test_controllers.py
+++ src/tests/test_controllers.py
@@ -23,7 +23,9 @@ Created by Aaron Crosman on 2015-01-24.
 """
 
 import unittest
-import logging, sys
+import logging
+import sys
+import re
 
 import mysql.connector
 
@@ -38,12 +40,13 @@ import tests.helper_functions
 
 
 class TestHistoriaCoreController(unittest.TestCase):
-    
+
     config_location = 'tests/test_config'
-    
+
     @classmethod
     def setUpClass(cls):
-        cls.config = tests.helper_functions.load_configuration(cls.config_location)
+        cls.config = tests.helper_functions.load_configuration(
+                                            cls.config_location)
 
     def setUp(self):
         self.testDBName = TestHistoriaCoreController.config['database']["main_database"]
@@ -54,9 +57,9 @@ class TestHistoriaCoreController(unittest.TestCase):
           'database': '',
           'raise_on_warnings': TestHistoriaCoreController.config['database']["raise_on_warnings"]
         }
-    
+
     def tearDown(self):
-        # Make a good faith effort to clean up any database we created along the way.
+        # Make a good faith effort to clean up any database we created
         try:
             db = core_data_objects.HistoriaDatabase(None)
             db.connection_settings = self.default_settings
@@ -64,55 +67,54 @@ class TestHistoriaCoreController(unittest.TestCase):
             cur = db.cursor()
             cur.execute("DROP DATABASE `{0}`".format(self.testDBName))
             db.commit()
-            cur.execute("DROP DATABASE `{0}`".format(self.user_db_name ))
+            cur.execute("DROP DATABASE `{0}`".format(self.user_db_name))
             db.commit()
         except Exception as err:
             pass
-            
+
         try:
-            cur.execute("DROP USER {0}@{1}".format(self.user_db_name[:16], 'localhost'))
+            cur.execute("DROP USER {0}@{1}".format(self.user_db_name[:16],
+                        'localhost'))
             cur.execute("FLUSH PRIVILEGES")
             self.db.commit()
         except Exception as err:
-            pass # This throws errors when the users doesn't exist.
-    
+            pass  # This throws errors when the users doesn't exist.
 
-    
     def test_00_construct(self):
         """HistoriaCoreController: Test __new__() and __init__()"""
-        
-        obj = controllers.HistoriaCoreController(config_location = 'tests/test_config')
-        
+
+        obj = controllers.HistoriaCoreController(config_location=
+                                                 'tests/test_config')
+
         self.assertIsInstance(obj.logger, logging.Logger, "Default logger isn't a logger")
         self.assertIsInstance(obj.database, system_db.HistoriaSystemDatabase, "Database didn't initalize to None when none provided")
         self.assertIsNone(obj.interface, "Interface didn't initalize to None when none provided")
         self.assertEqual(obj.active_users, {}, "Active users dict isn't just an empty Dictionary")
         self.assertEqual(obj.active_user_databases, {}, "Active dabases dict isn't just an empty Dictionary")
-    
+
     def test_10_create_sys_database(self):
         """HistoriaCoreController: create_database(self, database_name, connection_settings, db_type = "user")"""
-        # def create_database(self, database_name, connection_settings, db_type = "user"):
-        
+
         obj = controllers.HistoriaCoreController(config_location = 'tests/test_config')
-        
+
         db = obj.create_database(self.testDBName, self.default_settings, db_type="system")
-        
+
         self.assertIsInstance(db, system_db.HistoriaSystemDatabase)
         self.assertEqual(db.name, self.testDBName, "Database name isn't the value we provided")
         self.assertTrue(db.connected, "DB not connected after creating itself")
-        
+
         cur = db.cursor()
-        
+
         sql = "SHOW DATABASES;"
         cur.execute(sql)
         result = [tbl['Database'] for tbl in cur.fetchall()]
         self.assertIn(db.name, result, "My database doesn't appear to exist")
-        
+
         sql = "SELECT DATABASE();"
         cur.execute(sql)
         result = cur.fetchall()
         self.assertEqual(db.name, result[0]['DATABASE()'], "Database in use is not me")
-        
+
         sql = "SHOW TABLES;"
         cur.execute(sql)
         result = cur.fetchall()
@@ -121,10 +123,9 @@ class TestHistoriaCoreController(unittest.TestCase):
         self.assertEqual(len(result), len(db.member_classes), "Wrong number of tables in database")
         for tbl in result:
             self.assertIn(tbl[col], class_names,"Table {0} not in my table list.".format(tbl[col]))
-        
+
     def test_15_create_user_database(self):
         """HistoriaCoreController: create_database(self, database_name, connection_settings, db_type = "user")"""
-        # def create_database(self, database_name, connection_settings, db_type = "user"):
 
         obj = controllers.HistoriaCoreController(config_location = 'tests/test_config')
         sys_db = obj.create_database(self.testDBName, self.default_settings, db_type="system")
@@ -132,7 +133,7 @@ class TestHistoriaCoreController(unittest.TestCase):
         self.user_db_name = TestHistoriaCoreController.config['database']["user_database_name_prefix"] + 'userdb'
         obj.database = sys_db
         db = obj.create_database(self.user_db_name, self.default_settings, db_type="user")
-        
+
         self.assertIsInstance(db, user_db.HistoriaUserDatabase)
         self.assertEqual(db.name, self.user_db_name, "Database name isn't the value we provided")
         self.assertTrue(db.connected, "DB not connected after creating itself")
@@ -165,9 +166,9 @@ class TestHistoriaCoreController(unittest.TestCase):
 
         obj = controllers.HistoriaCoreController(config_location = 'tests/test_config')
         db = obj.create_database(self.testDBName, self.default_settings, db_type="user")
-        
+
         db2 = obj.load_database(self.testDBName)
-        
+
         self.assertIsInstance(db2, system_db.HistoriaUserDatabase, "Valid database object not returned.")
         self.assertTrue(db2.connected, "New database object not connected.")
         self.assertEqual(db2.name, self.testDBName, "New Database name doesn't match requested database name.")
@@ -177,23 +178,23 @@ class TestHistoriaCoreController(unittest.TestCase):
         # def authenticate_user(self, user_name, password):
         obj = controllers.HistoriaCoreController(config_location = 'tests/test_config')
         db = obj.create_database(self.testDBName, self.default_settings, db_type="system")
-        
+
         obj.database = db
-        
+
         name = "Sir Robin of Camelot"
         password = "What... is the capital of Assyria"
         email = "sir.robin@camelot.gov.uk"
-        
+
         u1 = user.HistoriaUser(db)
         u1.name = name
         u1.password = password
         u1.email = email
         u1.save()
-        
+
         response = obj.authenticate_user(name, password)
         self.assertIsInstance(response, user.HistoriaUser, "Valid user not returned with valid creds.")
         self.assertEqual(u1, response, "Returned user isn't the user that created the record.")
-        
+
         response = obj.authenticate_user(email, password)
         self.assertIsInstance(response, user.HistoriaUser, "Valid user not returned with valid creds.")
         self.assertEqual(u1, response, "Returned user isn't the user that created the record.")
@@ -211,10 +212,36 @@ class TestHistoriaCoreController(unittest.TestCase):
         """HistoriaCoreController: check_access(self, user, database)"""
         self.fail("nothing to access yet")
         # def check_access(self, user, database):
-    
+
     @unittest.expectedFailure
     def test_50_end_session(self):
         """HistoriaCoreController: end_session(self, user)"""
         # def end_session(self, user):
         self.fail("no sessions to end yet")
-        
+
+    def test_60_request_patterns(self):
+        """HistoriaCoreController: request_patterns(self, reset=True)"""
+        obj = controllers.HistoriaCoreController(
+                config_location='tests/test_config')
+
+        # Some valid URLs that should pass
+        valid_urls = ['system/user/login',
+                      'system/user/logout',
+                      'database/1/fetch/1',
+                      'database/2/update/1']
+        # Some invalid URLs that should fail
+        invalid_urls = ['total_junk',
+                        'database/JUNK/fetch/123',
+                        '../../../../directory/file',
+                        '..%u2216..%u2216directory/file',
+                        '..%2F..%2F..%2Fdirectory/file']
+
+        pat = obj.request_patterns(reset=True)
+
+        for url in valid_urls:
+            match = pat.match(url)
+            self.assertIsNotNone(match, "Failed to match: {0}".format(url))
+
+        for url in invalid_urls:
+            match = pat.match(url)
+            self.assertIsNone(match, "Matched: {0}".format(url))
